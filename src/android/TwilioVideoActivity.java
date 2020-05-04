c9c7ecb57ff2a0befddcb4e4ff494aca96d02539
ffortunato <filipelfortunato@gmail.com>
2020-05-04 13:10:04 +0100

    s

--- src/android/TwilioVideoActivity.java
+++ src/android/TwilioVideoActivity.java
@@ -17,7 +17,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
